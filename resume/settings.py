a1e4606e7b954897d4b82ad784e87045454ee82c
arunaadhi <s.arunadithyan@gmail.com>
2023-08-16 22:37:35 +0530

    added static

--- resume/settings.py
+++ resume/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/' #i changed from static/ to /static/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+# settings.py
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
@@ -131,6 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS={
     messages.ERROR :'danger'
 }
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  # New
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # New
