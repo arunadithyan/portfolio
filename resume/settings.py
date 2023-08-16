15eb81490a3589ecc2a6c3a6d557e1bfe8294faa
arunaadhi <s.arunadithyan@gmail.com>
2023-08-16 23:13:35 +0530

    add

--- resume/settings.py
+++ resume/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # settings.py
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
